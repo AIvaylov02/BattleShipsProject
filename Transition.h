123cca907262da58febf9d8001ec92e8b6084a55
AIvaylov02 <98240356+AIvaylov02@users.noreply.github.com>
2022-01-27 03:38:43 +0200

    Code refactoring

--- Transition.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-bool IsPrepared(unsigned int playerSmallShipsCount, unsigned int playerMediumShipsCount, unsigned int playerLargeShipsCount, unsigned int playerCruiserShipsCount);
-bool LockConfig(char response, unsigned int playerSmallShipsCount, unsigned int playerMediumShipsCount, unsigned int playerLargeShipsCount, unsigned int playerCruiserShipsCount);
